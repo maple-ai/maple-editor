38f43644518ca34369556ff39ced348525dd7ad5
Ali Abdul Wahid <aly.dihaw96@gmail.com>
2022-02-05 00:58:43 +0700

    init editor

---
+++ src/ckeditor.js
@@ -0,0 +1,172 @@
+/**
+ * @license Copyright (c) 2003-2022, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+// The editor creator to use.
+import InlineEditorBase from '@ckeditor/ckeditor5-editor-inline/src/inlineeditor';
+
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
+import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
+import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
+import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
+import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
+import Heading from '@ckeditor/ckeditor5-heading/src/heading';
+import Image from '@ckeditor/ckeditor5-image/src/image';
+import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
+import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
+import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
+import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
+import Indent from '@ckeditor/ckeditor5-indent/src/indent';
+import Link from '@ckeditor/ckeditor5-link/src/link';
+import List from '@ckeditor/ckeditor5-list/src/list';
+import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
+import Table from '@ckeditor/ckeditor5-table/src/table';
+import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
+import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
+import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices';
+
+import HeadingButtonsUI from '@ckeditor/ckeditor5-heading/src/headingbuttonsui';
+import ParagraphButtonUI from '@ckeditor/ckeditor5-paragraph/src/paragraphbuttonui';
+
+import ImageResizeEditing from '@ckeditor/ckeditor5-image/src/imageresize/imageresizeediting';
+import ImageResizeHandles from '@ckeditor/ckeditor5-image/src/imageresize/imageresizehandles';
+
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
+import SimpleUploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter';
+import wordcount from '@ckeditor/ckeditor5-word-count/src/wordcount';
+
+import Mention from "@ckeditor/ckeditor5-mention/src/mention";
+
+import TodoList from '@ckeditor/ckeditor5-list/src/todolist';
+
+import Emoji from './emoji';
+
+import EmojiPeople from "./emoji-people";
+import EmojiNature from "./emoji-nature";
+import EmojiFood from "./emoji-food";
+import EmojiActivity from "./emoji-activity";
+import EmojiObjects from "./emoji-objects";
+import EmojiPlaces from "./emoji-places";
+import EmojiSymbols from "./emoji-symbols";
+import EmojiFlags from "./emoji-flags";
+
+export default class InlineEditor extends InlineEditorBase {}
+
+// Plugins to include in the build.
+InlineEditor.builtinPlugins = [
+	Essentials,
+	UploadAdapter,
+	Autoformat,
+	Bold,
+	Italic,
+	BlockQuote,
+	CKFinder,
+	CloudServices,
+	EasyImage,
+	Heading,
+	Image,
+	ImageCaption,
+	ImageStyle,
+	ImageToolbar,
+	ImageUpload,
+	Indent,
+	Link,
+	List,
+	MediaEmbed,
+	Paragraph,
+	PasteFromOffice,
+	Table,
+	TableToolbar,
+	TextTransformation,
+
+	TodoList,
+
+	HeadingButtonsUI,
+	ParagraphButtonUI,
+	
+	ImageResizeEditing,
+	ImageResizeHandles,
+
+	Mention,
+	
+	Underline,
+	Alignment,
+	Emoji,
+	EmojiPeople,
+	EmojiNature,
+	EmojiPlaces,
+	EmojiFood,
+	EmojiActivity,
+	EmojiObjects,
+	EmojiSymbols,
+	EmojiFlags,
+	SimpleUploadAdapter,
+	wordcount
+];
+
+// Editor configuration.
+InlineEditor.defaultConfig = {
+	toolbar: {
+		items: [
+			'paragraph',
+			'heading2',
+			'|',
+			'bold',
+			'italic',
+			'underline',
+			'alignment',
+			'|',
+			'todoList',
+			'bulletedList',
+			'numberedList',
+			'insertTable',
+			'|',
+			'blockQuote',
+			'uploadImage',
+			'mediaEmbed',
+			'emoji',
+			'link',
+			'|',
+			'undo',
+			'redo'
+		]
+	},
+	heading: {
+		options: [
+			{ model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+			{ model: 'heading2', view: 'h2', title: 'Heading', class: 'ck-heading_heading' },
+		]
+	},
+	image: {
+		toolbar: [
+			'imageStyle:inline',
+			'imageStyle:block',
+			'imageStyle:side',
+			'|',
+			'toggleImageCaption',
+			'imageTextAlternative'
+		]
+	},
+	table: {
+		contentToolbar: [
+			'tableColumn',
+			'tableRow',
+			'mergeTableCells'
+		]
+	},
+	simpleUpload: {
+		uploadUrl: 'http://localhost:2000/api/v1/upload',
+		withCredentials: true,
+		headers: {
+			'API-KEY': 'API-KEY',
+		}
+	},
+	language: 'en'
+};
